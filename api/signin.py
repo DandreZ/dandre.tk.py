08d16989949255609129eea1ead1eb96a43ff5f6
DongZirui <122194489+DandreZ@users.noreply.github.com>
2023-01-29 13:18:13 +0800

    Add files via upload

--- api/signin.py
+++ api/signin.py
@@ -1,9 +1,10 @@
 from flask import Flask, request
 import pymongo
+app = Flask(__name__)
 @app.route('/signin', methods=['POST'])
 def gethtml():
     name=request.form['name']
     password=request.form['password']
     print(name,password)
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
