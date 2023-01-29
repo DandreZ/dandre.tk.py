6d50c4265d1d9e70bb5be5cc509cf620af3d3dbd
DongZirui <122194489+DandreZ@users.noreply.github.com>
2023-01-29 22:38:35 +0800

    Add files via upload

--- api/signin.py
+++ api/signin.py
@@ -5,6 +5,9 @@ import pymongo
 app = Flask(__name__)
 @app.route('/signin', methods=["POST","GET"])
 def gethtml():
+    global name
+    global email
+    global password
     name=request.form['name']
     email=request.form['email']
     password=request.form['password']
@@ -15,5 +18,5 @@ if __name__ == '__main__':
 AllClient=pymongo.MongoClient('mongodb+srv://root:dzr090315@dandre.g6y0ihm.mongodb.net/')
 UserDB=AllClient['user']
 UserCol=UserDB['account']
-NewAcc={"name":"%s","email":"%s","password":"%s"}
+NewAcc={"name":"%d"%name,"email":"%d"%email,"password":"%d"%password}
 UserCol.insert_one(NewAcc)
