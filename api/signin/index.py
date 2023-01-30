fa0a63b7d6682f4c3a013fbb340d6d5a563ecf89
DongZirui <122194489+DandreZ@users.noreply.github.com>
2023-01-30 13:30:53 +0800

    Add files via upload

--- api/signin/index.py
+++ api/signin/index.py
@@ -1,19 +1,21 @@
 from flask import Flask, request
 import pymongo
-app = Flask(__name__)
-@app.route('/api/signin', methods=["POST"])
-def gethtml():
-    global name
-    global email
-    global password
-    name=request.form['name']
-    email=request.form['email']
-    password=request.form['password']
-    print(name,email,password)
-    print('q')
-if __name__ == '__main__':
-    app.run()
-gethtml()
+name=1
+while name==1:
+    app = Flask(__name__)
+    @app.route('/api/signin', methods=["POST"])
+    def gethtml():
+        global name
+        global email
+        global password
+        name=request.form['name']
+        email=request.form['email']
+        password=request.form['password']
+        print(name,email,password)
+        print('q')
+    if __name__ == '__main__':
+        app.run()
+    gethtml()
 AllClient=pymongo.MongoClient('mongodb+srv://root:dzr090315@dandre.g6y0ihm.mongodb.net/')
 UserDB=AllClient['user']
 UserCol=UserDB['account']
