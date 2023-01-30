9291d96de815f9096f05f89b9e1e769637b8a1f2
DongZirui <122194489+DandreZ@users.noreply.github.com>
2023-01-30 11:45:45 +0800

    Add files via upload

--- api/signin.py
+++ api/signin.py
@@ -1,7 +1,7 @@
 from flask import Flask, request
 import pymongo
 app = Flask(__name__)
-@app.route('/', methods=["POST"])
+@app.route('/api', methods=["POST"])
 def gethtml():
     global name
     global email
